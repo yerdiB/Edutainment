db7e89ddb64d4952d0acbda9407ff81e8d9aac24
yerdi <yerdi.isman@gmail.com>
2023-01-16 23:04:10 +0600

    first commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Yerdaulet Ismanaliyev on 16.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
